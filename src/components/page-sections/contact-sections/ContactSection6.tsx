1ce3c4543d52fbc2ffc74de71e02f4c0bc416c3f
Y <duongy96@gmail.com>
2023-07-15 09:27:45 +0700

    sections/contact-sections

--- src/components/page-sections/contact-sections/ContactSection6.tsx
+++ src/components/page-sections/contact-sections/ContactSection6.tsx
@@ -1,9 +1,112 @@
 import { FC } from 'react';
 import { ContactSectionProps } from './types';
+import {
+  BugAntIcon,
+  ChatBubbleLeftRightIcon,
+  ComputerDesktopIcon,
+} from '@heroicons/react/24/outline';
 
 export const ContactSection6: FC<ContactSectionProps> = ({
   className,
   ...props
 }) => {
-  return <div className={` ${className}`} {...props}></div>;
+  return (
+    <div
+      className={`isolate bg-white px-6 py-24 sm:py-32 lg:px-8 ${className}`}
+      {...props}
+    >
+      <div className='mx-auto max-w-2xl sm:text-center'>
+        <h2 className='text-3xl font-bold tracking-tight text-neutral-900 sm:text-4xl'>
+          Contact sales
+        </h2>
+        <p className='mt-2 text-lg leading-8 text-neutral-600'>
+          Aute magna irure deserunt veniam aliqua magna enim
+          voluptate.
+        </p>
+      </div>
+      <div className='mx-auto mt-20 max-w-lg space-y-16'>
+        <div className='flex gap-x-6'>
+          <div className='flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-primary-600'>
+            <ChatBubbleLeftRightIcon
+              className='h-6 w-6 text-white'
+              aria-hidden='true'
+            />
+          </div>
+          <div>
+            <h3 className='text-base font-semibold leading-7 text-neutral-900'>
+              Sales support
+            </h3>
+            <p className='mt-2 leading-7 text-neutral-600'>
+              Ut cursus est ut amet. Lobortis eget egestas
+              leo vitae eget porttitor risus blandit. Nunc a
+              in lorem vel iaculis porttitor.
+            </p>
+            <p className='mt-4'>
+              <a
+                href='#'
+                className='text-sm font-semibold leading-6 text-primary-600'
+              >
+                Contact us{' '}
+                <span aria-hidden='true'>&rarr;</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className='flex gap-x-6'>
+          <div className='flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-primary-600'>
+            <BugAntIcon
+              className='h-6 w-6 text-white'
+              aria-hidden='true'
+            />
+          </div>
+          <div>
+            <h3 className='text-base font-semibold leading-7 text-neutral-900'>
+              Bug reports
+            </h3>
+            <p className='mt-2 leading-7 text-neutral-600'>
+              Expedita qui non ut quia ipsum voluptatum
+              ipsam pariatur. Culpa vitae ipsum minus eius
+              vero quo quibusdam.
+            </p>
+            <p className='mt-4'>
+              <a
+                href='#'
+                className='text-sm font-semibold leading-6 text-primary-600'
+              >
+                Report a bug{' '}
+                <span aria-hidden='true'>&rarr;</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className='flex gap-x-6'>
+          <div className='flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-primary-600'>
+            <ComputerDesktopIcon
+              className='h-6 w-6 text-white'
+              aria-hidden='true'
+            />
+          </div>
+          <div>
+            <h3 className='text-base font-semibold leading-7 text-neutral-900'>
+              Technical support
+            </h3>
+            <p className='mt-2 leading-7 text-neutral-600'>
+              Sint aut modi porro consequatur architecto
+              commodi qui consequatur. Dignissimos adipisci
+              minima.
+            </p>
+            <p className='mt-4'>
+              <a
+                href='#'
+                className='text-sm font-semibold leading-6 text-primary-600'
+              >
+                Join our Discord{' '}
+                <span aria-hidden='true'>&rarr;</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/page-sections/contact-sections.tsx
+++ src/pages/page-sections/contact-sections.tsx
@@ -1,5 +1,34 @@
+import { ContactSection1 } from '@/components/page-sections/contact-sections/ContactSection1';
+import { ContactSection2 } from '@/components/page-sections/contact-sections/ContactSection2';
+import { ContactSection3 } from '@/components/page-sections/contact-sections/ContactSection3';
+import { ContactSection4 } from '@/components/page-sections/contact-sections/ContactSection4';
+import { ContactSection5 } from '@/components/page-sections/contact-sections/ContactSection5';
+import { ContactSection6 } from '@/components/page-sections/contact-sections/ContactSection6';
+import { ContactSection7 } from '@/components/page-sections/contact-sections/ContactSection7';
+import { ContactSection8 } from '@/components/page-sections/contact-sections/ContactSection8';
 import MainTemplate from '@/templates/MainTemplate';
 
 export default function ContactSectionsPage() {
-  return <MainTemplate>Contact Sections</MainTemplate>;
+  return (
+    <MainTemplate>
+      Contact Sections Centered
+      <ContactSection1 />
+      Side-by-side grid
+      <ContactSection2 />
+      Consequat sunt cillum cillum elit sint. Qui occaecat
+      nisi in ipsum commodo.
+      <ContactSection3 />
+      Split with pattern
+      <ContactSection4 />
+      Split with pattern on dark
+      <ContactSection5 />
+      Simple four-column
+      <ContactSection6 />
+      Simple centered
+      <ContactSection7 />
+      With testimonial
+      <ContactSection8 />
+      Split with image
+    </MainTemplate>
+  );
 }
