ba7dcb778216309529017a7683b6f32364065bc1
Le Van Nhu Y <31586323+duongacy@users.noreply.github.com>
2023-07-27 00:01:20 +0700

    AlertDialog

--- src/pages/radix/index.tsx
+++ src/pages/radix/index.tsx
@@ -1,5 +1,6 @@
 import { RadixAccordion } from '@/radix/components/Accordion';
-import React from 'react';
+import { RadixAlertDialog } from '@/radix/components/AlertDialog';
+import { useState } from 'react';
 
 interface IRadixUIProps {}
 const radixAccordionItems = [
@@ -20,12 +21,31 @@ const radixAccordionItems = [
 const RadixUI: React.FunctionComponent<
   IRadixUIProps
 > = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const turnOffDialog = () => {
+    setDialogOpen(false);
+  };
+  const toggleDialog = () => {
+    setDialogOpen((prev) => !prev);
+  };
+
   return (
     <>
       <RadixAccordion
         items={radixAccordionItems}
         type='multiple'
         className='w-[300px]'
+      />
+      <button onClick={toggleDialog}>Toggle dialog</button>
+      <RadixAlertDialog
+        open={dialogOpen}
+        onOk={turnOffDialog}
+        onCancel={turnOffDialog}
+        okText='OK'
+        cancelText='Cancel'
+        title='Are you absolutely sure?'
+        description='This action cannot be undone. This will permanently delete your account and remove your
+        data from our servers.'
       />
     </>
   );
--- src/radix/components/AlertDialog.tsx
+++ src/radix/components/AlertDialog.tsx
@@ -1,9 +1,76 @@
-import * as React from 'react';
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
+import { FC, ReactNode, useRef, useEffect } from 'react';
 
-interface AlertDialogProps {}
+interface AlertDialogProps
+  extends AlertDialog.AlertDialogProps {
+  trigger?: ReactNode;
+  cancelText?: string;
+  okText?: string;
+  onCancel?: () => void;
+  onOk?: () => void;
+  title?: ReactNode;
+  description?: ReactNode;
+}
 
-export const AlertDialog: React.FC<AlertDialogProps> = (
-  props
-) => {
-  return <></>;
+export const RadixAlertDialog: FC<AlertDialogProps> = ({
+  trigger,
+  okText,
+  cancelText,
+  onCancel,
+  onOk,
+  title,
+  description,
+  children,
+  ...props
+}) => {
+  return (
+    <AlertDialog.Root {...props}>
+      {trigger && (
+        <AlertDialog.Trigger asChild>
+          {trigger}
+        </AlertDialog.Trigger>
+      )}
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay className='bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0' />
+        <AlertDialog.Content className='data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'>
+          {title && (
+            <AlertDialog.Title className='m-0 text-[17px] font-medium text-mauve12'>
+              {title}
+            </AlertDialog.Title>
+          )}
+          {description && (
+            <AlertDialog.Description className='mb-5 mt-4 text-[15px] leading-normal text-mauve11'>
+              {description}
+            </AlertDialog.Description>
+          )}
+          {children}
+          {(!!cancelText || !!okText) && (
+            <div className='flex justify-end gap-[25px]'>
+              {cancelText && (
+                <AlertDialog.Cancel asChild>
+                  <button
+                    onClick={onCancel}
+                    className='inline-flex h-[35px] items-center justify-center rounded-[4px] bg-mauve4 px-[15px] font-medium leading-none text-mauve11 outline-none hover:bg-mauve5 focus:shadow-[0_0_0_2px] focus:shadow-mauve7'
+                  >
+                    {cancelText}
+                  </button>
+                </AlertDialog.Cancel>
+              )}
+
+              {okText && (
+                <AlertDialog.Action asChild>
+                  <button
+                    onClick={onOk}
+                    className='text-red11 bg-red4 hover:bg-red5 focus:shadow-red7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]'
+                  >
+                    {okText}
+                  </button>
+                </AlertDialog.Action>
+              )}
+            </div>
+          )}
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
+  );
 };
